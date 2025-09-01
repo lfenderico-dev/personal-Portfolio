ef60d4ab56a715ce756cb48f88dcf0c2c992940a
Lorenzo Fenderico <lorenzoFenderico.dev@gmail.com>
2025-09-01 08:22:13 +0200

    First part layout projects section

---
+++ src/components/NewProject.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="flex flex-col gap-12">
+    <div class="flex flex-col gap-8">
+      <div class="grid-cols-2 grid gap-2">
+        <div class="bg-neutral-400/70 w-fit px-8 rounded-full">{{ tag1 }}</div>
+        <div class="bg-neutral-400/70 w-fit px-8 rounded-full">{{ tag2 }}</div>
+        <div class="bg-neutral-400/70 w-fit px-8 rounded-full">{{ tag3 }}</div>
+      </div>
+
+      <h1 class="text-3xl font-bold">{{ Title }}</h1>
+      <p>{{ Description }}</p>
+
+      <div class="flex flex-row">
+        <a :href="siteLink">Live site</a>
+        <a :href="githubLink">Github</a>
+      </div>
+
+      <h1>Year: {{ Year }}</h1>
+    </div>
+
+    <div>
+      <img :src="projectImg" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  tag1?: string
+  tag2?: string
+  tag3?: string
+  Title: string
+  Description: string
+  siteLink: string
+  githubLink: string
+  Year: number
+  projectImg: string
+}>()
+</script>
+
+<style scoped></style>
--- src/components/Sections/ProjectsSection.vue
+++ src/components/Sections/ProjectsSection.vue
@@ -1,7 +1,40 @@
 <template>
-  <div></div>
+  <div class="h-fit flex flex-col gap-4 my-[10%] mx-[10%] lg:mx-[5%] lg:my-[15%]">
+    <div class="flex flex-row items-center gap-4">
+      <hr class="border-1 rounded-full inline w-[20%] bg-black" />
+      <h1
+        class="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl text-center font-bold w-fit inline"
+      >
+        My works
+      </h1>
+    </div>
+
+    <div>
+      <NewProject v-for="project in projects" :key="project.id" v-bind="project" />
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NewProject from '../NewProject.vue'
+
+import sliceDiceImg from '@/assets/photo/projectImg/slice&DiceImg.webp'
+
+const projects = [
+  {
+    id: 1,
+    tag1: 'VueJs',
+    tag2: 'Tailwindcss',
+    tag3: 'Typescript',
+    Title: 'Slice&Dice',
+    Description:
+      'Slice and Dice is a pizza restaurant that makes fresh, delicious pizzas with bold flavors. Visit our website to check out the menu, order online, and see our latest deals. Great pizza is just a click away!',
+    siteLink: 'https://sliceanddice-pizza.netlify.app/',
+    githubLink: 'https://github.com/lfenderico-dev/Slice-Dice.git',
+    Year: 2025,
+    projectImg: sliceDiceImg,
+  },
+]
+</script>
 
 <style scoped></style>
